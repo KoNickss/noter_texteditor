2fa2069e192be2e560bc532e802c871fde68bd56
KoNicks <61112636+KoNickss@users.noreply.github.com>
2020-11-23 20:39:26 +0200

    Add files via upload

---
+++ CMakeFiles/noter.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/noter_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/noter_autogen.dir/ParseCache.txt"
+  "noter_autogen"
+  "CMakeFiles/noter.dir/main.cpp.o"
+  "CMakeFiles/noter.dir/noter.cpp.o"
+  "CMakeFiles/noter.dir/noter_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/noter_en_001.ts_lst_file"
+  "noter"
+  "noter.pdb"
+  "noter_en_001.ts"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/noter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/noter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for noter.
+# This may be replaced when dependencies are built.
